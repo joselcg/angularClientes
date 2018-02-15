ca4dd6bcab83cb55e6134e3f35ad717bf26a9e24
Jose LUis de la CRuz <joseluisdelacruzgarrido@gmail.com>
2018-02-15 20:57:15 +0100

    First commit

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,9 @@
+import { Cliente } from './cliente';
+export const CLIENTES: Cliente [] = [
+  {id: 1, nombre: 'Jose Luis', apellido: 'de la Cruz', email: 'email@email.com', createAt: '15.02.2017'},
+  {id: 2, nombre: 'Jacinto', apellido: 'Perez', email: 'email@email.com', createAt: '15.02.2017'},
+  {id: 3, nombre: 'Manuel', apellido: 'Moreno', email: 'email@email.com', createAt: '15.02.2017'},
+  {id: 4, nombre: 'Lorenzo', apellido: 'Garcia', email: 'email@email.com', createAt: '15.02.2017'},
+  {id: 5, nombre: 'Macario', apellido: 'Olmedo', email: 'email@email.com', createAt: '15.02.2017'},
+  {id: 6, nombre: 'Mateo', apellido: 'Garrido', email: 'email@email.com', createAt: '15.02.2017'}
+];
